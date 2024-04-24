8eeac48bdc6fcb7727c07f23c4786495c5ab2c4c
Michael Schroeder <mls@suse.de>
2024-04-24 10:36:10 +0200

    Move pgpDigParams_s definition into the header file
    So that we can split away code.

--- rpmpgp_internal.c
+++ rpmpgp_internal.c
@@ -42,43 +42,6 @@ typedef struct pgpPktSigV4_s {
     uint8_t hashlen[2];	/*!< length of following hashed material. */
 } * pgpPktSigV4;
 
-/** \ingroup rpmio
- * Values parsed from OpenPGP signature/pubkey packet(s).
- */
-struct pgpDigParams_s {
-    uint8_t tag;
-    char * userid;		/*!< key user id */
-    uint8_t key_flags;		/*!< key usage flags */
-
-    uint8_t version;		/*!< key/signature version number. */
-    uint32_t time;		/*!< key/signature creation time. */
-    uint8_t pubkey_algo;	/*!< key/signature public key algorithm. */
-
-    uint8_t hash_algo;		/*!< signature hash algorithm */
-    uint8_t sigtype;
-    uint8_t * hash;
-    uint32_t hashlen;
-    uint8_t signhash16[2];
-    pgpKeyID_t signid;		/*!< key id of pubkey or signature */
-    uint32_t key_expire;	/*!< key expire time. */
-    uint32_t sig_expire;	/*!< signature expire time. */
-    int revoked;		/*!< is the key revoked? */
-    uint8_t saved;		/*!< Various flags. */
-#define	PGPDIG_SAVED_TIME	(1 << 0)
-#define	PGPDIG_SAVED_ID		(1 << 1)
-#define	PGPDIG_SAVED_KEY_FLAGS	(1 << 2)
-#define	PGPDIG_SAVED_KEY_EXPIRE	(1 << 3)
-#define	PGPDIG_SAVED_PRIMARY	(1 << 4)
-#define	PGPDIG_SAVED_VALID	(1 << 5)
-#define	PGPDIG_SAVED_SIG_EXPIRE	(1 << 6)
-    uint8_t * embedded_sig;	/* embedded signature */
-    size_t embedded_sig_len;	/* length of the embedded signature */
-    pgpKeyID_t mainid;		/* key id of main key if this is a subkey */
-
-    size_t mpi_offset;		/* start of mpi data */
-    pgpDigAlg alg;		/*!< algorithm specific data like MPIs */
-};
-
 static rpmpgpRC getKeyID(const uint8_t *h, size_t hlen, pgpKeyID_t keyid);
 
 static inline unsigned int pgpGrab2(const uint8_t *s)
--- rpmpgp_internal.h
+++ rpmpgp_internal.h
@@ -38,7 +38,47 @@ struct pgpDigAlg_s {
     void *data;			/*!< algorithm specific private data */
 };
 
+/*
+ * Values parsed from OpenPGP signature/pubkey packet(s).
+ */
+struct pgpDigParams_s {
+    uint8_t tag;
+    char * userid;		/*!< key user id */
+    uint8_t key_flags;		/*!< key usage flags */
+
+    uint8_t version;		/*!< key/signature version number. */
+    uint32_t time;		/*!< key/signature creation time. */
+    uint8_t pubkey_algo;	/*!< key/signature public key algorithm. */
+
+    uint8_t hash_algo;		/*!< signature hash algorithm */
+    uint8_t sigtype;
+    uint8_t * hash;
+    uint32_t hashlen;
+    uint8_t signhash16[2];
+    pgpKeyID_t signid;		/*!< key id of pubkey or signature */
+    uint32_t key_expire;	/*!< key expire time. */
+    uint32_t sig_expire;	/*!< signature expire time. */
+    int revoked;		/*!< is the key revoked? */
+    uint8_t saved;		/*!< Various flags. */
+#define	PGPDIG_SAVED_TIME	(1 << 0)
+#define	PGPDIG_SAVED_ID		(1 << 1)
+#define	PGPDIG_SAVED_KEY_FLAGS	(1 << 2)
+#define	PGPDIG_SAVED_KEY_EXPIRE	(1 << 3)
+#define	PGPDIG_SAVED_PRIMARY	(1 << 4)
+#define	PGPDIG_SAVED_VALID	(1 << 5)
+#define	PGPDIG_SAVED_SIG_EXPIRE	(1 << 6)
+    uint8_t * embedded_sig;	/* embedded signature */
+    size_t embedded_sig_len;	/* length of the embedded signature */
+    pgpKeyID_t mainid;		/* key id of main key if this is a subkey */
+
+    size_t mpi_offset;		/* start of mpi data */
+    pgpDigAlg alg;		/*!< algorithm specific data like MPIs */
+};
+
+RPM_GNUC_INTERNAL
 pgpDigAlg pgpDigAlgNewPubkey(int algo, int curve);
+
+RPM_GNUC_INTERNAL
 pgpDigAlg pgpDigAlgNewSignature(int algo);
 
 #endif /* _RPMPGP_INTERNAL_H */
